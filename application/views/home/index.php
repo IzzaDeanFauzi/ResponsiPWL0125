477b478506c4473d3a475abea35c58525bf3d25b
IzzaDeanFauzi <izza.fauzi@students.amikom.ac.id>
2018-12-28 15:45:28 +0700

    aku ganteng

---
+++ application/views/home/index.php
@@ -0,0 +1,11 @@
+<div class="container mt-3">
+	<div class="jumbotron">
+	  <h1 class="display-4">Selamat Menikmati Project Kami</h1>
+	  <p class="lead">selalu rendah hati dan tidak sombong</p>
+	  <hr class="my-4">
+	  <p>Semoga Dapat Jodoh Mba pemrograman Web</p>
+	  <p class="lead">
+	    <a class="btn btn-primary btn-lg" href="<?= base_url(); ?>members" role="button">Lanjut gan</a> 
+	  </p>
+	</div>
+</div>
---
+++ application/views/templates/header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/bootstrap.css" > 
+    <!-- font Awesome -->
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/plugins/font_awesome/css/all.css">
+    <!-- My Style -->
+    <!-- <link rel="stylesheet" href="<?= base_url(); ?>assets/css/style.css">  -->
+    <title><?= $judul?></title>
+  </head>
+  <body>
+    <div class="container mt-4 bg-light">
+      <a class="btn" href=" <?= base_url(); ?> ">Home <span class="sr-only">(current)</span></a>
+      <a class="btn" href=" <?= base_url(); ?>members ">Members</a>
+      <a class="btn " href=" <?= base_url(); ?>city ">City</a>
+      <a class="btn " href=" <?= base_url(); ?>company ">Company</a>
+      
+    </div>
